cd46a34a3cfa13202e8b329e59bb8ab32e9d9e47
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-06-23 14:13:42 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist_name
+  
   @@all = []
 
   def self.all
@@ -8,6 +9,18 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    new_song = self.new
+    @@all << new_song
+    new_song
+  end
+
+  def self.new_by_name(name)
+    @name = name
+    song_w_name = self.new
+song_w_name.name
   end
 
 end
