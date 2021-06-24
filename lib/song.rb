01ebe0557ac25ba0d0719aeb614e18364a44f5b9
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-06-23 18:00:38 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -25,6 +25,7 @@ class Song
   end
 
   def self.create_by_name(name)
+
     create_name = self.new
     create_name.name = name
     create_name.save
@@ -32,17 +33,59 @@ class Song
   end
 
   def self.find_by_name(name)
+
     self.all.find{|i| i.name == name}
+    
   end
 
   def self.find_or_create_by_name(song)
-    if self.all.include?(song)
-      self.find_by_name(song)
+    found_song = Song.find_by_name(song)
+    if found_song 
+      found_song
     else
       self.create_by_name(song)
     end
-       # binding.pry
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |a|
+    a.name
+    end
+  end
+
+  def self.new_from_filename(file)
+    split_file = file.split(" - ")
+    artist_name = split_file[0]
+    song_name = split_file[1].chomp(".mp3")
+    new_file = self.new
+    new_file.name = song_name
+    new_file.artist_name = artist_name
+    new_file
 
   end
 
-end
+  def self.create_from_filename(file)
+     #binding.pry
+     splitted_f = file.split(" - ")
+     artist_name_2 = splitted_f[0]
+     song_name_2 = splitted_f[1].chomp(".mp3")
+     #file has been split up
+     file_creation = self.create_by_name(file)
+     #new instance is saved
+     file_creation.name = song_name_2
+     file_creation.artist_name = artist_name_2
+     file_creation
+
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  end
+
+#song_1 = Song.find_or_create_by_name("songname")
+#song_2 = Song.find_or_create_by_name("songname2")
+#song_3 = Song.find_or_create_by_name("songname3")
+#song_4 = Song.find_or_create_by_name("songname4")
+
